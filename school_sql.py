eef282815105430d5d997e18328149daf0773446
root <hrundhaug@arizona.edu>
2025-04-05 14:01:04 -0700

    testing commit again

--- school_sql.py
+++ school_sql.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
 
+import cgi
+import os
+import sys
+
+path = os.environ.get('PATH_INFO', '')
+method = os.environ.get('REQUEST_METHOD', '')
+
+pathSplit = path.split('/')
+
+splitstring = ""
+for chunk in pathSplit:
+    splitstring += chunk + " "
+splitstring += method
+
+print("Status: 200");
+print("Content-Type: text/plain")
+print()
+print(" this is the body of the response")
+print(splitstring)
+
+
