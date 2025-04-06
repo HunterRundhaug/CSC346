dd0c605a669362e097275ca1670476e100711a92
root <hrundhaug@arizona.edu>
2025-04-06 14:07:13 -0700

    first query to db

---
+++ db_config.py
@@ -0,0 +1,10 @@
+DB_CONFIG = {
+    'driver': '{ODBC Driver 18 for SQL Server}',
+    'server': 'school-sql-server.database.windows.net,1433',
+    'database': 'school_sqlDB',
+    'username': 'hunter@school-sql-server',
+    'password': 'Williewoo1',
+    'encrypt': 'yes',
+    'trust_cert': 'no',
+    'timeout': '30'
+}
--- school_sql.py
+++ school_sql.py
@@ -1,27 +1,44 @@
-#!/usr/bin/env python3
+#!/venv/bin/python
 
 import cgi
 import os
 import sys
+import pyodbc
+from db_config import DB_CONFIG
 
 path = os.environ.get('PATH_INFO', '')
 method = os.environ.get('REQUEST_METHOD', '')
 
 def main():
-    pathSplit = path.split('/')
+    pathSplit = path.strip('/').split('/')
 
-    if len(pathSplit) < 1:
+    if not pathSplit or pathSplit[0] == '':
         invalidRequest()
         return
 
     if(pathSplit[0] == "students"): # a request regarding students
 
         if(method == "GET"): # get students
-            print("Status: 200")
-            print("Content-Type: text/plain")
-            print()
-            print(" GET STUDENTS ")
-            pass
+            try:
+                conn = connectToDB()
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM students")
+                rows = cursor.fetchall()
+                students = ""
+
+            except Exception as e:
+                print("Status: 500 Internal Server Error")
+                print("Content-Type: text/plain\n")
+                print(f"Database error: {e}")
+
+            for row in rows:
+                 # row[0] is id, row[1] is name
+                students += f"ID: {row[0]}, Name: {row[1]}\n"
+
+            # CGI headers
+            print("Status: 200 OK")
+            print("Content-Type: text/plain\n")
+            print(students)                  
 
         elif method == "POST": # post students ??
             pass
@@ -39,7 +56,21 @@ def invalidRequest():
     print("Status: 400 Bad Request")
     print("Content-Type: text/plain")
     print()
-    print("no path provided")
+    print(f'no path provided:{path}')
+
+def connectToDB():
+    conn = pyodbc.connect(
+        f'DRIVER={DB_CONFIG["driver"]};'
+        f'SERVER={DB_CONFIG["server"]};'
+        f'DATABASE={DB_CONFIG["database"]};'
+        f'UID={DB_CONFIG["username"]};'
+        f'PWD={DB_CONFIG["password"]};'
+        f'Encrypt={DB_CONFIG["encrypt"]};'
+        f'TrustServerCertificate={DB_CONFIG["trust_cert"]};'
+        f'Connection Timeout={DB_CONFIG["timeout"]};'
+    )
+
+    return conn
 
 
 main()
